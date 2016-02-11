de3403b0fad4100d268fedd0740db296ce461ddc
aesahetr <antoine.borreda@gmail.com>
2016-02-11 21:20:18 +0100

    Début Calcul

---
+++ Selection2016/Selection2016/Data/OrderCost.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Selection2016.Data
+{
+  public class OrderCost : IComparable
+  {
+    public int OrderID { get; set; }
+    public decimal CompletionProduct { get; set; }
+    public int DroneCost { get; set; }
+    public int CostOrder { get; set; }
+
+    public int CompareTo(object obj)
+    {
+      var orderCost = obj as OrderCost;
+      int compare = this.CompletionProduct.CompareTo(orderCost.CompletionProduct);
+      if (compare == 0)
+      {
+        compare = (orderCost.DroneCost + orderCost.CostOrder).CompareTo(this.DroneCost + this.CostOrder);
+      }
+
+      return compare;
+    }
+  }
+}
---
+++ Selection2016/Selection2016/Data/Ordonanceur.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Selection2016.Data
+{
+  public class Ordonanceur
+  {
+    Grid Grid { get; set; }
+    List<Drone> Drones { get; set; }
+    List<Warehouse> Entrepots { get; set; }
+    List<Order> Commandes { get; set; }
+    List<Product> Produits { get; set; }
+    
+    // TODO liste d'ordres ??
+
+    public void MainLoop()
+    {
+      do
+      {
+        // Pour chaque entrepot
+        //   Evaluation du cout d'une commande
+        // Pour chaque commande
+        //   sélection des meilleurs instructions
+        foreach (var entrepot in Entrepots)
+        {
+          var listeCost = new List<OrderCost>();
+          var currentStock = entrepot.CloneRepartitionProduit();
+          foreach (var order in Commandes)
+          {
+            OrderCost oc = new OrderCost();
+            oc.CostOrder = order.Lignes.Sum( p => Math.Min(p.QteCommandee, currentStock[p.Produit])) / order.Lignes.Sum(p => p.QteCommandee);
+          
+          }
+        }        
+      }while (Grid.TourCourrant < Grid.NbTours);
+    }
+  }
+}
